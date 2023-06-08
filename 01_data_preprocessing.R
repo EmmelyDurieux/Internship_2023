826f78c7d08f940bd976095436db94ab62dd3193
EmmelyDurieux <118811307+EmmelyDurieux@users.noreply.github.com>
2023-06-08 11:38:06 +0300

    cleaned up code

--- 01_data_preprocessing.R
+++ 01_data_preprocessing.R
@@ -1,167 +1,165 @@
+# ### DATA PREPROCESSING OF BIOM TABLE AND METADATA FOR AUTOENCODER ###########
 # getting the climate zone from kgc based on latitude and longitude
 # performing descriptive statistics to filter out samples
+
 # loading and/or installing packages
 pkg <- installed.packages()[, "Package"]
 if(!('kgc' %in% pkg)) {install.packages("kgc")}
-library(fastverse)
+if(!('data.table' %in% pkg)) {install.packages("data.table")}
+if(!('stringr' %in% pkg)) {install.packages("stringr")}
+
 library(data.table)
 library(kgc)
 library(rbiom)
+library(stringr)
 
-# read in EMP data
-# data: https://github.com/biocore/emp/tree/master/data/mapping-files
-df<-read.delim("emp_qiime_mapping_subset_2k.tsv", sep="\t")
-DT <- setDT(df)
+# files and variables ---------------------------------------------------------#
+# metadata: https://github.com/biocore/emp/tree/master/data/mapping-files
+# biomtable: (https://github.com/biocore/emp/tree/master/data/biom-tables)
 
-# lookup the climate zones
-zonesDT <- data.table(DT, 
-                      rndCoord.lon = RoundCoordinates(DT$longitude_deg),
-                      rndCoord.lat = RoundCoordinates(DT$latitude_deg))
-zonesDT <- data.table(zonesDT, ClimateZ=LookupCZ(zonesDT))
-zonesDT
+sample_meta <- "data/emp_qiime_mapping_subset_2k.tsv"
+biom_table <- "data/emp_deblur_90bp.subset_2k.biom"
+empo3_ontology <- "Soil (non-saline)"
+abundance_threshold <- 1e-04
+prevalence_threshold <- 0.1
 
-# filtering and grouping data
-# limit to only soil (non-saline) data
-zonesDT_soil <- zonesDT[empo_3=="Soil (non-saline)"]
+# read in EMP meta data + assign climate zones --------------------------------#
+sam <- fread(sample_meta)
+sam_subset <- sam[which(empo_3 == empo3_ontology), ]
 
-# how many samples in each climate zone
-zonesDT_soil[,count(empo_3), by=ClimateZ][order(freq)]
+sam_subset <- data.table(sam_subset, 
+                   rndCoord.lon = RoundCoordinates(sam_subset$longitude_deg),
+                   rndCoord.lat = RoundCoordinates(sam_subset$latitude_deg))
+sam_subset <- data.table(sam_subset, ClimateZ=LookupCZ(sam_subset))
 
-# which samples belong to the less represented climatezone
-zonesDT_soil[ClimateZ=="BWh"| ClimateZ=="BSh" | ClimateZ=="Cwb", .(X.SampleID)]
 
-# dropping climate zones with less than 3 representing samples
-zonesDT_soil <- zonesDT_soil[ClimateZ!="BWh"& ClimateZ!="BSh" & ClimateZ!="Cwb"]
-
-# filter on type of sample
-zonesDT_soil[, count(sample_taxid), by=sample_scientific_name]
-zonesDT_soil[sample_scientific_name=="rhizosphere metagenome", .(X.SampleID)]
-
-# looking at the country
-zonesDT_soil[,count(country)]
-zonesDT_soil[, count(country), by=ClimateZ][order(x)]
-
-# looking at sequence depth
-zonesDT_soil[, count(depth_m)]
-
-# amount of observed OTU's per zone
-zonesDT_soil[, sum(adiv_observed_otus), by=ClimateZ]
-zonesDT_soil[, .(sum=sum(adiv_observed_otus)), by=ClimateZ][order(sum)]
-
-# filter for samples that have 0 observations_deblur
-zonesDT_soil[observations_deblur_90bp==0, .(X.SampleID)]
-zonesDT_soil[observations_deblur_100bp==0, .(X.SampleID)]
-zonesDT_soil[observations_deblur_150bp==0, .(X.SampleID)]
-
-# sum observations_deblur per climate zone
-zonesDT_soil[observations_deblur_90bp!=0,.(sum=sum(observations_deblur_90bp)), 
-             by=ClimateZ][order(sum)]
-zonesDT_soil[observations_deblur_100bp!=0,.(sum=sum(observations_deblur_100bp)), 
-             by=ClimateZ][order(sum)]
-zonesDT_soil[observations_deblur_150bp!=0,.(sum=sum(observations_deblur_150bp)), 
-             by=ClimateZ][order(sum)]
-
-# write filtered data.table to file
-fwrite(zonesDT_soil, file = "Kgc_soil_filtered.tsv", sep = "\t")
-
-# looking at ESVs in the biom file
-# reading EMP data (https://github.com/biocore/emp/tree/master/data/biom-tables)
-biom2 <- read.biom("emp_deblur_90bp.subset_2k.biom")
-print(biom2)
-
-# store biom file as data.table file 
-# DTbiom = counts(biom2) %>% as.data.frame(biom2) %>% setDT(biom2, keep.rownames = TRUE)
-biom_counts = counts(biom2)
-DFbiom = as.data.frame(biom_counts)
-DTbiom = setDT(DFbiom, keep.rownames = TRUE) # Taxa will become an extra column
-
-# checking the data.table
-str(DTbiom)
-dim(DTbiom)
+# read and filter biom file ---------------------------------------------------#
+biom2 <- read_biom(biom_table, tree = FALSE)
 
 # limiting data to only soil (non-saline samples)
-subset_DTbiom <- DTbiom[, colnames(DTbiom) %in% zonesDT_soil$X.SampleID, with=FALSE]
+subset_biom <- select(biom2, samples = sam_subset$`#SampleID`)
 
-# checking if it matches up to the zonesDT_soil sampleIDs
-str(subset_DTbiom)
-colnames(subset_DTbiom)
+# obtain counts and taxonomy biom file
+taxonomy_table  <- taxonomy(subset_biom)
+ab_counts <- counts(subset_biom)
+ab_counts = setDT(as.data.frame(ab_counts), keep.rownames = "TaxaIDabv")
 
-# looking at amount of ESV's per sample
-# for example the first sample in the data.table
-DTbiom[, sum(DTbiom$"1883.2008.269.Crump.Artic.LTREB.main.lane2.NoIndex")]
-# 77249 ESV's
 
-# now for every sample in the subset biom data.table
-# Apply sum function to every column of subset_DTbiom
+# filtering ESV's -------------------------------------------------------------#
+# relative abundances (threshold : >0.01%) + presenting in more than 10% of samples
 
-# create empty data.frame
-df_sum <- data.frame(X.SampleID = character(), ESV_sum = numeric())
+# relative abundances 
 
-# loop over columns and calculate sum
-for (i in colnames(subset_DTbiom)){
-  col_sum <- sum(subset_DTbiom[[i]])
-  new_row <- data.frame(X.SampleID = i, ESV_sum = col_sum)
-  df_sum <- rbind(df_sum, new_row)
-}
-
-# print result
-print(df_sum)
-df_sum <- setDT(df_sum)
-# note: the sampleID are not sorted the way they are in zonesDT_soil
-df_sum_ordered <- df_sum[][order(df_sum$X.SampleID)]
-
-# checking if these numbers make sense
-subset_DTbiom[subset_DTbiom$"808.FL.3.16a.s.4.1.sequences"!=0]
-subset_DTbiom[subset_DTbiom$"808.FL.3.16a.s.4.1.sequences"!=0, sum(subset_DTbiom$"808.FL.3.16a.s.4.1.sequences")]
-
-# combining the ESV sum with the climatezones
-combi <- merge(zonesDT_soil, df_sum_ordered, by="X.SampleID")
-
-# looking at the amount of ESV's per climate zone
-combi[,.(sum=sum(ESV_sum)), by=ClimateZ][order(sum)]
-
-# which ESV's are not present in any sample
-# taking a look at the samples
-subset_DTbiom$"808.FL.3.16a.s.4.1.sequences"
-# The dataset is parse and there are a lot of EVs that aren't present
-# select rows with sum equal to 0
-zero_rows <- subset_DTbiom[rowSums(subset_DTbiom) == 0]
-
-# print result
-print(zero_rows)
-# according to this, there are 251468 rows where rowSum = 0
-
-# add column indicating whether each row has sum equal to 0
-subset_DTbiom[, has_zero_sum := rowSums(subset_DTbiom) == 0]
-
-str(subset_DTbiom)
-
-# adding back the taxa sequence 
-subset_DTbiom <- data.table(DTbiom$rn, subset_DTbiom)
-
-# select rows with sum not equal to 0
-no_zero_rows <- subset_DTbiom[has_zero_sum==FALSE]
-
-# other option of filtering ESV's
-# relative abundances (threshold : >0.001%) + presenting in more than 10% of samples
-# calculating relative abundances
-
-# calculate column sums
-col_sums_except_first <- colSums(subset_DTbiom[, -1, with = FALSE])
-
-# calculate proportions and scaling factor
-prop <- subset_DTbiom[, -1]/ col_sums_except_first
-scaling_factor <- 100
-
-# calculate relative abundances
-rel_abundances <- prop * scaling_factor
+rel_abundance_table <- ab_counts[, lapply(.SD, function(x) x/sum(x, na.rm = TRUE)),
+                      .SDcols = colnames(ab_counts[,-1])]
 
 # adding back the taxa sequence
-rel_abundances <- data.table(subset_DTbiom$V1, rel_abundances)
+rel_abundance_table <- data.table(ab_counts$TaxaIDabv, rel_abundance_table)
 
-# filtering out rows
-# 10% samples: >= 13 samples need to have rel_abundance >0.001 
-filter_rows <- rel_abundances[rowSums(rel_abundances >= 0.001) >= 13]
+# removing noise (rel_abundance < 0.01)
+rel_abundance_table[rel_abundance_table < abundance_threshold] <- 0
 
-# write to file
-fwrite(filter_rows, file = "Rel_abundance_BIOM_filtered.tsv", sep = "\t")
+
+# prevalence
+row_filter <- rowSums(rel_abundance_table > 0) > floor(prevalence_threshold * nsamples(subset_biom))
+rel_abundance_table <- rel_abundance_table[row_filter, ]
+abs_abundance_table <- ab_counts[row_filter,]
+
+
+# filtering taxonomy table ----------------------------------------------------#
+taxonomy_table <- setDT(
+  as.data.frame(taxonomy_table),
+  keep.rownames = "TaxaID"
+)
+
+taxonomy_table <- taxonomy_table[row_filter,]
+
+taxonomy_table$TaxaIDabv = rleid(taxonomy_table$TaxaID, prefix = "Taxa")
+
+taxonomy_table = taxonomy_table[, c(
+  "TaxaID",
+  "TaxaIDabv",
+  "Kingdom",
+  "Phylum",
+  "Class",
+  "Order",
+  "Family",
+  "Genus",
+  "Species"
+), with = FALSE]
+
+index <- match(rel_abundance_table$TaxaIDabv, taxonomy_table$TaxaID)
+
+rel_abundance_table$TaxaIDabv <- taxonomy_table[index, ]$TaxaIDabv
+
+abs_abundance_table$TaxaIDabv <- taxonomy_table[index, ]$TaxaIDabv
+
+taxonomy_table <- taxonomy_table[which(Kingdom == "k__Bacteria"), ]
+abundance_table <- abs_abundance_table[which(TaxaIDabv %in% taxonomy_table$TaxaIDabv), ]
+rel_abundance_table <- rel_abundance_table[which(TaxaIDabv %in% taxonomy_table$TaxaIDabv)]
+
+# filter samples based on representation --------------------------------------#
+
+# drop missing values
+sam_subset = sam_subset[which(ClimateZ != "" & !is.na(ClimateZ)), ]
+
+
+index = colSums(obs0[, sam_subset$`#SampleID`, with = FALSE])
+
+drop = names(which(index == 0))
+keep = names(which(index != 0))
+
+sam_subset = sam_subset[which(sam_subset$`#SampleID` %in% keep), ]
+
+
+# filter out climate zones with less then representing 3 samples  
+
+sam_subset[, by = ClimateZ, N_climatezone := .N]
+
+sam_subset = sam_subset[which(N_climatezone >= 3), ]
+
+sam_subset$N_climatezone = NULL
+
+# creating mapping files ----------------------
+
+sam_mapping <- data.table(
+  "SampleIDabv" = paste0("S", 1:nrow(sam_subset)),
+  "SampleID"    = sam_subset$`#SampleID`
+)
+
+
+sam_subset = merge(sam_mapping, sam_subset, by.x = "SampleID", by.y = "#SampleID")
+sam_subset = sam_subset[str_order(sam_subset$SampleIDabv, numeric = TRUE), ]
+
+# relative abundance table
+rel_abundance_table = rel_abundance_table[, c("TaxaIDabv", sam_subset$SampleID), with = FALSE]
+
+colnames(rel_abundance_table) = c("TaxaIDabv", sam_subset$SampleIDabv)
+
+# absolute abundance table
+abundance_table = abundance_table[, c("TaxaIDabv", sam_subset$SampleID), with = FALSE]
+
+colnames(abundance_table) = c("TaxaIDabv", sam_subset$SampleIDabv)
+
+# write to to file ------------------------------------------------------------#
+
+fwrite(
+  sam_subset, paste0(workdir, "/sample-metadata.", empo3_ontology, ".txt"),
+  row.names = FALSE, quote = FALSE, sep = "\t"
+)
+
+fwrite(
+  abundance_table, paste0(workdir, "/abundance-table.", empo3_ontology, ".txt"),
+  row.names = FALSE, quote = FALSE, sep = "\t"
+)
+
+fwrite(
+  rel_abundance_table, paste0(workdir, "/rel-abundance-table.", empo3_ontology, ".txt"),
+  row.names = FALSE, quote = FALSE, sep = "\t"
+)
+
+fwrite(
+  taxonomy_table, paste0(workdir, "/taxonomy-table.", empo3_ontology, ".txt"),
+  row.names = FALSE, quote = FALSE, sep = "\t"
+)
+
